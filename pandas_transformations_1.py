7d2c798204ef6579371b945fed2c7471449dcac9
ArnabDutta027 <addyrocks.arnab@gmail.com>
2025-08-28 17:22:19 +0530

    eval,ast.literal_eval, json_normalize

--- pandas_transformations_1.py
+++ pandas_transformations_1.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from ast import literal_eval
+import ast
+import json
 
 def multiply_values(rows):
     return rows["calories"] * rows["duration"]
@@ -71,3 +74,60 @@ for rows in product_numpy_array:
     row_list = rows.reshape(-1).tolist()
     type_rows = type(rows)
     print(f"PRODUCT - {row_list[0]} ,SALES - {row_list[1]}, SALES_SQUARE - {row_list[2]}, SALES_MULTIPLIED - {row_list[3]}, ADD_SALES_SQUARE_AND_SALES_MULTIPLIED - {row_list[4]} , TYPE OF ROWS - {type_rows}")
+
+#eval
+tot="4+3"
+print(eval(tot))
+
+sample_json_string="""{"name": "John", "age": 30, "city": "New York"}"""
+print(eval(sample_json_string))
+
+sample_json_string_1 = """{
+"order_num" : "O2012019231a",
+"order_date" : "2012-06-27",
+"order_id" : 21934,
+"order_item" : [
+{
+"product_id" : 20933,
+"quantity" : 3,
+"price" : 36000,
+"product_name" : "Thingamagic 2000",
+"unit_price" : 12000
+},
+{
+"product_id" : 10366,
+"quantity" : 1,
+"price" : 100,
+"product_name" : "Super Duper Blooper",
+"unit_price" : 100
+}
+]
+}"""
+print(eval(sample_json_string_1))
+#ast.literal_eval
+
+
+sample_json_string_2 = """{"coord":{"lon":-0.1257,"lat":51.5085},
+"weather":[{"id":801,"main":"Clouds","description":"few clouds","icon":"02d"}],
+"base":"stations",
+"main":{"temp":292.86,"feels_like":287.94,"temp_min":292.04,"temp_max":293.71,"pressure":1027,"humidity":45},
+"visibility":10000,"wind":{"speed":6.17,"deg":260},"clouds":{"all":23},"dt":1617027097,"sys":{"type":1,"id":1414,"country":"GB","sunrise":1616996538,"sunset":1617042474},"timezone":3600,"id":2643743,"name":"London","cod":200}"""
+parsed_Value= ast.literal_eval(sample_json_string_2)
+print(parsed_Value.keys())
+#pd.json_normalize
+
+json.loads(sample_json_string_2)
+df = pd.json_normalize(json.loads(sample_json_string_2),max_level=1)
+print(df.head())
+print(df.columns)
+
+
+df_1 = pd.json_normalize(ast.literal_eval(sample_json_string_2),max_level=1)
+print(df_1.head())
+print(df_1.columns)
+
+
+
+df_2 = pd.json_normalize(eval(sample_json_string_2),max_level=1)
+print(df_2.head())
+print(df_2.columns)
